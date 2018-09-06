9c093b113953fc40b71e90438d7031274c1a9a2c
Joel Holmes <holmes89@gmail.com>
2018-09-05 20:01:50 -0400

    added flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"golang-serverless-and-server/hello"
 	"net/http"
 
@@ -10,12 +11,16 @@ import (
 
 func main() {
 	server := flag.Bool("server", false, "run as server")
+	flag.Parse()
+
 	hello.RegisterRoutes()
 	if *server {
+		fmt.Println("Running as server on port 3000")
 		if err := http.ListenAndServe(":3000", nil); err != nil {
 			panic(err)
 		}
 	} else {
+		fmt.Println("Running as lambda on port 3000")
 		if err := gateway.ListenAndServe(":3000", nil); err != nil {
 			panic(err)
 		}
